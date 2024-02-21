e31903a05a3503eb1558374b72116e913398c65e
asutarisucu <asutaricusu.s@gmail.com>
2024-02-21 09:17:45 +0900

    表示切替の実装

---
+++ farm_item.js
@@ -0,0 +1,5 @@
+export const item={
+    "1":"iron",
+    "2":"gold",
+    "3":"slime"
+}
---
+++ script.js
@@ -0,0 +1,40 @@
+//最初に読み込みで変数全部持ってくる
+import item from './farm_item.js'
+window.onload = function(){
+    const selectElement = document.getElementById('select_type');
+    const div_farm = document.getElementById("farm");
+    const div_mod = document.getElementById("mod")
+    selectElement.onchange=type.bind(null,selectElement,div_farm,div_mod)
+    type(selectElement,div_farm,div_mod)
+
+    const select_farm_item=document.getElementById("select_farm_item")
+    create_select(select_farm_item)
+    console.log('load complete!');
+}
+
+//selectによってfarmかmodの表示を切り替える
+function type(selectElement,div_farm,div_mod) {
+    const search_value=selectElement.value;
+    if(search_value==="defult"){
+        div_farm.style.display="none";
+        div_mod.style.display="none";
+    }
+    else if(search_value==="farm"){
+        div_farm.style.display="block";
+        div_mod.style.display="none";
+    }else if(search_value==="mod"){
+        div_farm.style.display="none";
+        div_mod.style.display="block";
+    }
+}
+
+//farmの要素をtaglistから生成する
+function create_select(farm_item){
+    const item_list=JSON.parse(item);
+    const list_length=Object.keys(item_list).length
+    for(let i=1;i<=list_length;i++){
+        let option = document.createElement("option");
+    option.setAttribute("vulue",item_list.i)
+    }
+    
+}
